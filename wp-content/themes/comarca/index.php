9673e7ca3b9ded9203b6c6bd1a5c24d195933c04
dudusbarros <joseeduardobarros@gmail.com>
2015-09-11 10:31:01 -0300

    Removing Test Plugins

--- wp-content/themes/comarca/index.php
+++ wp-content/themes/comarca/index.php
@@ -33,9 +33,9 @@ get_header(); ?>
 
 		// Previous/next page navigation.
 		the_posts_pagination( array(
-			'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
-			'next_text'          => __( 'Next page', 'twentyfifteen' ),
-			'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
+			'prev_text'          => __( 'Previous page', 'comarca' ),
+			'next_text'          => __( 'Next page', 'comarca' ),
+			'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'comarca' ) . ' </span>',
 		) );
 
 	// If no content, include the "No posts found" template.
