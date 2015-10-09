0404a1d664c22de1acc69d2e61aeb9e7bdb89350
dudusbarros <joseeduardobarros@gmail.com>
2015-10-09 09:09:22 -0300

    Add Villa

--- wp-content/themes/comarca/header.php
+++ wp-content/themes/comarca/header.php
@@ -11,8 +11,8 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport"
 	      content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/villa.min.js"></script>
-	<link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/villa.min.css">
+	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/dist/js/villa.min.js"></script>
+	<link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dist/css/villa.min.css">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<!--[if lt IE 9]>
--- wp-content/themes/comarca/index.php
+++ wp-content/themes/comarca/index.php
@@ -7,44 +7,58 @@
 
 get_header(); ?>
 
-<main id="main" class="site-main" role="main">
+<?php if ( is_home() && ! is_front_page() ) : ?>
 
-	<?php if ( have_posts() ) : ?>
+	<header class="main-header">
 
-		<?php if ( is_home() && ! is_front_page() ) : ?>
-			<header>
-				<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-			</header>
-		<?php endif; ?>
+		<div class="container">
 
-		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
+			<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
 
-			/*
-			 * Include the Post-Format-specific template for the content.
-			 * If you want to override this in a child theme, then include a file
-			 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-			 */
-			get_template_part( 'content', get_post_format() );
+		</div>
 
-		// End the loop.
-		endwhile;
+	</header>
 
-		// Previous/next page navigation.
-		the_posts_pagination( array(
-			'prev_text'          => __( 'Previous page', 'comarca' ),
-			'next_text'          => __( 'Next page', 'comarca' ),
-			'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'comarca' ) . ' </span>',
-		) );
+<?php endif; ?>
 
-	// If no content, include the "No posts found" template.
-	else :
-		get_template_part( 'content', 'none' );
+<main id="main" class="main" role="main">
 
-	endif;
-	?>
+	<div class="container">
 
-</main><!-- .site-main -->
+		<?php if ( have_posts() ) : ?>
+
+			<?php
+			// Start the loop.
+			while ( have_posts() ) : the_post();
+
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'content', get_post_format() );
+
+			// End the loop.
+			endwhile;
+
+			// Previous/next page navigation.
+			the_posts_pagination( array(
+				'prev_text'          => __( 'Previous page', 'comarca' ),
+				'next_text'          => __( 'Next page', 'comarca' ),
+				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'comarca' ) . ' </span>',
+			) );
+
+		// If no content, include the "No posts found" template.
+		else :
+			get_template_part( 'content', 'none' );
+
+		endif;
+		?>
+
+	</div>
+
+</main><!-- .main -->
+
+
 
 <?php get_footer(); ?>
